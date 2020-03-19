9fd4f44a9b5cb60c5b870c29a2ad9a5027e05e8d
ssmatos <sebastiansoto.fms@gmail.com>
2020-03-19 17:40:09 -0400

    .

---
+++ Customization2/Customization2/Customization2.Android/Renderers/MyEntryDroid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Customization2.Controls;
+using Customization2.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryDroid))]
+namespace Customization2.Droid.Renderers
+{
+	public class MyEntryDroid : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+				Control.SetBackground(null);
+		}
+
+		public MyEntryDroid(Context context) : base(context)
+		{
+		}
+	}
+}
---
+++ Customization2/Customization2/Customization2.Android/Services/OrientationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Customization2.Droid.Services;
+using Customization2.Services;
+using Xamarin.Forms.Internals;
+
+[assembly: Xamarin.Forms.Dependency(typeof(OrientationService))]
+namespace Customization2.Droid.Services
+{
+	public class OrientationService : IOrientation
+	{
+		public DeviceOrientation GetOrientation()
+		{
+			IWindowManager windowManager = Android.App.Application.Context.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
+
+			SurfaceOrientation orientation = windowManager.DefaultDisplay.Rotation;
+			bool isLandscape = orientation == SurfaceOrientation.Rotation90 ||
+				orientation == SurfaceOrientation.Rotation270;
+			return isLandscape ? DeviceOrientation.Landscape : DeviceOrientation.Portrait;
+		}
+	}
+}
